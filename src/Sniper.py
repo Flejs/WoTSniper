bada533cdca1362aec7da4e0fbd5ddb42f0ad1e0
Flej <139654819+Flejs@users.noreply.github.com>
2024-04-07 18:09:12 +0200

    Update Sniper.py

--- src/Sniper.py
+++ src/Sniper.py
@@ -1,32 +1,29 @@
-import time
-
 import aiohttp
 import asyncio
+import time
 
 async def checker(nickname, session):
-    url = f'https://api.worldoftanks.eu/wot/account/list/?application_id=demo&search={nickname}'
-    time.sleep(0.5)
+    url = f'https://eu.wargaming.net/personal/account/nicknames/{nickname}/'
     async with session.get(url) as response:
         if response.status == 200:
             data = await response.json()
-            if data.get('data')[0]['nickname'] == nickname:
+            if data.get('spa_id') is None:
                 print(f'Username {nickname} is available.')
             else:
                 print(f'Username {nickname} is not available.')
-        elif response.status == 409:  # Pokud je status kód 409, považuje se jméno také za dostupné
-            print(f'Username {nickname} is available due to status code 409.')
         else:
             print(f'Failed to fetch data for {nickname}: Status code {response.status}')
+    await asyncio.sleep(1)  # Adds an asynchronous delay after each request.
 
 async def main():
-    # Načte soubor s přezdívkami
+    # Loads the file with nicknames
     with open('nickname.txt', 'r') as f:
         nicknames = f.read().strip().split('\n')
 
-    # Vytvoří session pro asynchronní požadavky
+    # Creates a session for asynchronous requests
     async with aiohttp.ClientSession() as session:
         tasks = [checker(nickname, session) for nickname in nicknames]
         await asyncio.gather(*tasks)
 
-# Spustí asynchronní smyčku
+# Starts the asynchronous loop
 asyncio.run(main())
